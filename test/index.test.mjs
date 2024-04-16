c88c86c46b86e67fd4ef16ed178b01b3c8f5704b
Theo Sun <theo.sun.github@outlook.com>
2024-04-16 20:46:13 +0800

    fix: ut

--- test/index.test.mjs
+++ test/index.test.mjs
@@ -2,10 +2,8 @@ import { equal } from "node:assert";
 import { describe, it } from "node:test";
 import { a } from "../src/index.mjs";
 
-describe('Test Suite', () => {
-
-  it('should equal to 1', () => {
+describe("Test Suite", () => {
+  it("should equal to 1", () => {
     equal(a, 1);
   });
-
 });
