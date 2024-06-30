9469bcedead0996b046007f1af8cfde22b0040e2
Theo Sun <theo.sun.github@outlook.com>
2024-06-30 19:51:31 +0800

    chore: update

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 import js from "@eslint/js";
+import globals from "globals";
 
 export default [
   js.configs.recommended,
@@ -9,5 +10,10 @@ export default [
       "no-var": "error",
     },
     ignores: ["node_modules", "lib"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
   },
 ];
